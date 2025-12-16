304a9c82f2eed8d91d06fddf784eb9724d67aa3c
JSPG <jspg@tier1.es>
2025-12-16 16:26:31 +0100

    cambio de path en la ruta

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,7 +4,7 @@ import { ViewMonitoringErrorsInput } from './view-monitoring-errors_input/view-m
 import { ViewMonitoringErrorsOutput } from './view-monitoring-erros-output/view-monitoring-errors';
 
 export default [
-    { path: 'crud', component: Crud, data: { breadcrumb: "View Errors" } },
+    { path: 'view-errors', component: Crud, data: { breadcrumb: "View Errors" } },
     { path: 'view-monitoring-errors-input', component: ViewMonitoringErrorsInput, data: { breadcrumb: 'Monitor Input' }},
     { path: 'view-monitoring-errors-output', component: ViewMonitoringErrorsOutput, data: { breadcrumb: 'Monitor Output' }}
 ] as Routes;
